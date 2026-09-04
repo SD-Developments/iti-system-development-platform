a764cc83520d5450c4099c45f9c12b63a9fa6123
omarwaled8008 <omarwaled8888@gmail.com>
2026-09-04 17:02:12 +0300

    test: husky config 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import viteLogo from './assets/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
