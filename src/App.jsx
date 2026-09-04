1f74a6083e44180b07328075b7562f297815a6cf
omarwaled8008 <omarwaled8888@gmail.com>
2026-09-04 16:53:57 +0300

    test husky config

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0)
   const [count2, setCount2] = useState(0)
+  const [count3, setCount3] = useState(0)
 
   return (
     <>
