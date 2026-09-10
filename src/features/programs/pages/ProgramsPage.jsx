0b81d297b1970b014ecbbf268997898da359386d
omarwaled8008 <omarwaled8888@gmail.com>
2026-09-10 11:47:49 +0300

    test2

--- src/features/programs/pages/ProgramsPage.jsx
+++ src/features/programs/pages/ProgramsPage.jsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 const ProgramsPage = () => {
   return (
     <>
-      <h1 className="bg-lime-400 text-black w-3 mx-auto my-5">Programs Page</h1>
+      <h1 className="bg-lime-400 text-black w-3 mx-auto my-5">
+        Programs Page
+        <Button variant="stle1">fkasjfilas</Button>
+      </h1>
     </>
   );
 };
