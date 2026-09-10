7eec8b7b392e0a7f40b85003b94d77c5a2a2e08f
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2026-09-11 00:32:13 +0300

    refactor: remove unused header and Button component from ProgramsPage for cleaner code

--- src/features/programs/pages/ProgramsPage.jsx
+++ src/features/programs/pages/ProgramsPage.jsx
@@ -1,14 +1,5 @@
-import { Button } from '@/components/ui/button';
-
 const ProgramsPage = () => {
-  return (
-    <>
-      <h1 className="bg-lime-400 text-black w-3 mx-auto my-5">
-        Programs Page
-        <Button variant="stle1">fkasjfilas</Button>
-      </h1>
-    </>
-  );
+  return <></>;
 };
 
 export default ProgramsPage;
