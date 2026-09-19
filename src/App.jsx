918fd90b34eda9fac7fc260111fc28a422dc6dfe
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2026-09-19 21:48:01 +0300

    fix: update import path for NewsAndActivites component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ import ProjectsPage from './features/projects/pages/ProjectsPage';
 import EventsPage from './features/events/pages/EventsPage';
 import IntakesPage from './features/intakes/pages/IntakesPage';
 import MainLayout from './layouts/MainLayout';
-import NewsAndActivites from './features/newsandactivites/NewsAndActivites';
+// import NewsAndActivites from './features/newsandactivites/NewsAndActivites';
 import Lenis from 'lenis';
+import NewsAndActivites from './features/newsandactivites/page/NewsAndActivites';
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(SplitText);
 const lenis = new Lenis({
